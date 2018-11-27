79f2b8fe158dca3c2e13d53428d42ccdc18c6aee
cledman <cledman@gmail.com>
2018-11-27 21:16:24 -0200

    atualizando index

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require('express')
-const app = express()//aqui ele cria a aplicação
+const app = express()
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const categorias = require('./routes/categorias')
 const publicacoes = require('./routes/publicacoes')
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded()) // ele utiliza para poder pegar os dados que são passados via URL
+app.use(bodyParser.urlencoded()) 
 
-const port = process.env.PORT || 3000 //aqui ele consegue pegar a porta dinamicamente. É útil num projeto real, onde o provedor pode fornecer isso pra gente. Caso não consiga, usa a porta 3000
+const port = process.env.PORT || 3000
 
 app.get('/', async(request, response) =>{
 
     response.render('index')
-    /*
-    i++
-    response.send(`<h1>Olá Fullstack lab ${i}   </h1>`)//usando essa aspas, podemos incorporar variáveis ali. Isso é o chamado Template String.
-    */
+
 })
 
-app.use('/categorias', categorias)// essas categorias vêm agrupadas daqui: const categorias = require('./routes/categorias')
+app.use('/categorias', categorias)
 app.use('/publicacoes', publicacoes)
 
 app.listen(port, (err) =>{
